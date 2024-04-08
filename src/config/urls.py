561fb1bca823c102ab375d9414827922239601d5
Dmytro Parfeniuk <parfeniukink@gmail.com>
2024-04-08 21:52:24 +0300

    upload lesson cbv

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,17 +1,26 @@
 from django.contrib import admin
 from django.urls import path
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            token_obtain_pair)
+from rest_framework_simplejwt.views import TokenObtainPairView  # noqa
+from rest_framework_simplejwt.views import token_obtain_pair  # noqa
 
-from issues.api import create_issue, get_issues, retrieve_issue
+from issues.api import IssuesAPI, IssuesRetrieveUpdateDeleteAPI
 from users.api import create_user
+
+# HTTP GET /issues
+# HTTP POST /issues
+# HTTP GET /issues/ID
+# HTTP PUT /issues/ID
+# HTTP PATCH /issues/ID
+# HTTP DELETE /issues/ID
+
+# HTTP PUT /issues/ID/close
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", create_user),
-    path("issues/", get_issues),
-    path("issues/<int:issue_id>", retrieve_issue),
-    path("issues/create", create_issue),
+    path("issues/", IssuesAPI.as_view()),
+    path("issues/<int:id>", IssuesRetrieveUpdateDeleteAPI.as_view()),
     # Authentication
     # path("auth/token/", token_obtain_pair),
     path("auth/token/", TokenObtainPairView.as_view()),
