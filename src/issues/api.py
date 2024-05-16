45e9eda0b3686c75e3313153bb73eaba2eaa2a5b
Dmytro Parfeniuk <parfeniukink@gmail.com>
2024-05-16 20:22:56 +0300

    🐛 interpreter issues are fixed

--- src/issues/api.py
+++ src/issues/api.py
@@ -6,7 +6,6 @@ from users.enums import Role
 
 from .enums import Status
 from .models import Issue, Message
-from . import openapi
 
 
 class IssueSerializer(serializers.ModelSerializer):
@@ -23,7 +22,6 @@ class IssueSerializer(serializers.ModelSerializer):
         attrs["status"] = Status.OPENED
         return attrs
 
-    @openapi.schemas.user_create
     def get(self, request, *args, **kwargs):
         """Get issues from the database."""
         return super().get(request, *args, **kwargs)
--- src/issues/openapi/serializers.py
+++ src/issues/openapi/serializers.py
@@ -3,3 +3,4 @@ from rest_framework import serializers
 
 class UserCreateSerializer(serializers.Serializer):
     # username = serialiezrs.CharField(max_l...)
+    pass
