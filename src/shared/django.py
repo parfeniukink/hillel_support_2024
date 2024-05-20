78f83e8da450b43328fcea720d6f697370af6703
Dmytro Parfeniuk <parfeniukink@gmail.com>
2024-05-20 18:55:16 +0300

    timestamp field update. pagniation

--- src/shared/django.py
+++ src/shared/django.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class TimestampMixin(models.Model):
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True, null=True)
 
     class Meta:
         abstract = True
